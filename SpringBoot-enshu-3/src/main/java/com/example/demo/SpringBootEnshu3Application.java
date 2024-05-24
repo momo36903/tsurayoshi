56cb02296e39f1eb0826aecbc691b63a94376374
FJ2-0288\Owner <aaa>
2024-05-24 15:18:17 +0900

    first commit

---
+++ SpringBoot-enshu-3/src/main/java/com/example/demo/SpringBootEnshu3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnshu3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEnshu3Application.class, args);
+	}
+
+}
